b0ace739643e537a406fd9a41df3e1cdabcba747
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-06 13:29:24 -0700

    Create 141.rb
    141 exercise

---
+++ 141.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Finish annotating the code below.
+x = 0                                  # 1
+names = ["Saron", "Majora", "Danilo"]  # 2
+names[1] = "Bob"                       # 3
+x = names[2]                           # 4
+names[0] = x                           # 5
+pp names                               # 6
+
+# 1: x is 0
+# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
+# 3: "Bob" will be inserted between "Sharon" and "Majora"
+# 4: x is "Majora"
+# 5: "Sharon" is x
+# 6: ["Sharon", "Majora", "Danilo"]
+
+
+# QUESTION 2
+# Find and fix the mistakes in the code below.
+items = [3, 2, 3, 2]
+pp items
