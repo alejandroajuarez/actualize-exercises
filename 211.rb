c2d4f7fdd9e41047f39e2deb9346ce18e6178edc
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-09 18:11:18 -0700

    Add Exercise solutions

---
+++ 211.rb
@@ -0,0 +1,33 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# For the following problems, I was able to solve these from memory. The only one that I was a tad confused on was 'upcase'. I thought it was either
+# 'uppercase' or 'capitalize' but I also knew the method was a bit funny looking and it made sense once I saw the answer and redid the problem
+# from scratch.
+
+# For the next skills I will be working on tomorrow, I will be completing 7 - 10 in the variables1 branch. I do not want to rush these and would
+# like to take my time so i can make sure they are solidified in my mind like it was explained in the overview video, about not crunching all
+# of the exercises.
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "Heisenberg Titanic Hindenburg"
+puts word.reverse
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 2
+puts number * 10
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "Hello"
+word2 = " James"
+
+puts word1.upcase + word2.upcase
