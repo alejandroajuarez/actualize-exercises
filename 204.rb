53f5f442c24a4d3b0716eee40f06b8744d93b6d5
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-10 19:49:21 -0700

    Job Hacking Career Inventory

--- 204.rb
+++ 204.rb
@@ -5,4 +5,5 @@
 # Change the sharing settings of the document to "Anyone with the link can view" 
 # Copy the link to your Career Inventory Google document and paste it below.
 
-
+# Link to my Career inventory
+# https://docs.google.com/document/d/13XwgY19uIDKJjg6oQ20CCJPyLsyc08qIAo2_oMdG_JQ/edit?usp=sharing
