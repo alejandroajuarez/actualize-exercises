510de67898b14518708b542799935aee2bd9729d
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-10 19:58:00 -0700

    4 Deliberate Practice Problems

---
+++ 221.rb
@@ -0,0 +1,20 @@
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word3 = "howmanylettersareinthisstring"
+puts word3.length
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer
+
+num3 = 2.55
+puts num3.to_i
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+num4 = 29
+num5 = 39
+puts num4 * num5
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+lowerword = "FJDKSFDSF"
+puts lowerword.downcase
