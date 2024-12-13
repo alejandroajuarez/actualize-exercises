a5975ae6d3142eb023601ecb6c1cfc47c988340f
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-12 18:01:03 -0700

    Add exercise solution

---
+++ 242.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+array1 = ["hello", "Goodbye", "huh?"]
+array1.push("Who?")
+pp array1
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+
+hash1 = { "adjective" => "Word that describes or defines a noun", "noun" => "Word that refers to a thing"}
+hash1["verb"] = "A word used to describe an action"
+puts hash1
