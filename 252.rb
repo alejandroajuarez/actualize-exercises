250e7eaf6c6bf02139b9923684e6e5fd4ae60d19
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-13 18:36:25 -0700

    Add exercise solutions

---
+++ 252.rb
@@ -0,0 +1,30 @@
+# QUESTION 1
+# Write a method that takes in four numbers and returns the numbers added together.
+# Then run the method and print the result. 
+# (Note: you do not need to make a custom class for this exercise, just a method)
+
+puts "Please write 4 separate numbers"
+num1 = gets.chomp.to_i
+num2 = gets.chomp.to_i
+num3 = gets.chomp.to_i
+num4 = gets.chomp.to_i
+
+result = num1 + num2 + num3 + num4
+
+puts "The result is " + result.to_s
+
+# QUESTION 2
+# Write a class called Shoe that stores attributes for name, color, and price.
+# Then make an instance of a shoe and print out the details using `pp`.
+# (Note: for this problem, just make the initialize method in the class, no other methods needed)
+
+class Shoe
+  def initialize(input_name, input_color, input_price)
+    @name = input_name
+    @color = input_color
+    @price = input_price
+  end
+end
+
+shoe = Shoe.new("Hoka", "Black/White", 124.99)
+pp shoe
