e83db1c24171615eea28bf59360ae0cdfa3a3b7e
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-15 11:25:32 -0700

    Revised 252 exercise

--- 252.rb
+++ 252.rb
@@ -3,16 +3,17 @@
 # Then run the method and print the result. 
 # (Note: you do not need to make a custom class for this exercise, just a method)
 
-puts "Please write 4 separate numbers"
-num1 = gets.chomp.to_i
-num2 = gets.chomp.to_i
-num3 = gets.chomp.to_i
-num4 = gets.chomp.to_i
+def sum
 
-result = num1 + num2 + num3 + num4
+  puts "Please write 4 separate numbers"
+  num1 = gets.chomp.to_i
+  num2 = gets.chomp.to_i
+  num3 = gets.chomp.to_i
+  num4 = gets.chomp.to_i
 
-puts "The result is " + result.to_s
-
+  result = num1 + num2 + num3 + num4
+end
+  puts "The result is " + sum.to_s
 # QUESTION 2
 # Write a class called Shoe that stores attributes for name, color, and price.
 # Then make an instance of a shoe and print out the details using `pp`.
@@ -23,8 +24,8 @@ class Shoe
     @name = input_name
     @color = input_color
     @price = input_price
-  end
-end
+  end  # ends the initialize method
+end   # ends the class definition
 
 shoe = Shoe.new("Hoka", "Black/White", 124.99)
 pp shoe
