16500d71d1cae0d9e3687d8d6b0cfa0c9e1c8834
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-20 19:45:20 -0700

    add exercise solution

---
+++ 354.rb
@@ -0,0 +1,36 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Ruby has a .sort method that makes it easy to sort an array. 
+# The goal of this exercise is to implement your own sort from scratch 
+# (without using Ruby's #sort method!)
+
+# The approach you will implement is called bubble sort. 
+# First you examine the first two items in the array. 
+# If the second is smaller than the first, you swap their positions. 
+# Then you move on to the next two items and keep repeating. 
+# You can see a visual of it in action here: https://upload.wikimedia.org/wikipedia/commons/c/c8/Bubble-sort-example-300px.gif 
+
+# Your job is to implement a bubble sort that takes an array and returns a sorted array. 
+# To be clear, you're not allowed to use the built in .sort method.
+# Instead you'll write code to go through the array two items at a time 
+# to accomplish the same thing.
+
+# To create an array
+array = []
+
+# To print an array's contents
+puts array
+
+# To push items into an array
+array << "hello"
+
+# To select the first item in an array
+array = ["Hello", "Bye"]
+
+puts array[0]
+
+# THis is where I am stuck on this problem. After having my array with numbers in it, I will write my sort method that will push the
+# bubble-sorted array into a second array. I will come back and correct this after turning this version in after looking up how to complete this.
