433c0b799682d34f9fd6658a21b1dee96d016078
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-20 19:17:33 -0700

    saved file

---
+++ 344.rb
@@ -0,0 +1,18 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Given an integer, write a method to return its roman numeral representation.
+# (1 is I, 2 is II, 3 is III, 4 is IV, 5 is V, 6 is VI, 7 is VII, etc.)
+# (You can see an example of all the rules here: https://byjus.com/maths/roman-numerals/#chart)
+
+# How to ask for a users input
+puts "Please type in a number"
+
+# How to get a users input
+input = gets.chomp
+
+# Without looking anything up, I am unsure of the type of loop I would need to use for this type of problem. I think that I'd need a sort of 'for each'
+# loop but I am not sure. In that instance I would say that for each specific number entered, give an output of a roman numeral. I feel very
+# stuck on this problem off the top of my head.
