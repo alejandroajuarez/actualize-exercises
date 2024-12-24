92d890c067d4299292bb067cb9c8bc609144cb18
alejandrojuarez117 <aljuarez117@gmail.com>
2024-12-24 15:02:54 -0700

    add exercise solution

---
+++ 424.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Read about the Ruby map method: https://www.rubyguides.com/2018/10/ruby-map-method/ 
+# Then write a program that uses the map method with an array of numbers 
+# to create a new array with each number multiplied by 100.
+
+array = [2, 4, 6]
+
+p array.map { |n| n * 100 }
